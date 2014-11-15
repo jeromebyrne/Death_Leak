ee38567682e9c081cb00c7ee101b51bd468e929d
jeromebyrne <jeromebyrne21@hotmail.com>
2014-11-15 03:59:52 -0800

    - reverted the Render() back to sync up with the Update() function and can see much smoother visuals.

--- Game/src/program.cpp
+++ Game/src/program.cpp
@@ -38,41 +38,14 @@ int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdL
         return 0;
 
 	MSG mssg;                // message from queue
-	// LONGLONG cur_time;       // current time
 	float  target_delta = 1.0f/60.0f; 
-	//LONGLONG perf_cnt;       // performance timer frequency
-	//BOOL perf_flag=FALSE;    // flag determining which timer to use
-	//LONGLONG next_time=0;    // time to render next frame
 
 	Timing::Instance()->SetTargetDelta(target_delta);
 
-	double last_update_time = 0;
+	double last_update_time = 0.0;
 	bool first_update = true;
 
 	timeBeginPeriod(1); // set timing resolution to 1 millisecond
-
-	/*double freq;
-    unsigned __int64 base_time; // what happens if hardware doesnt support this?
-
-	// is there a performance counter available?
-	if (QueryPerformanceFrequency((LARGE_INTEGER *) &perf_cnt)) 
-	{
-		// yes, set time_count and timer choice flag
-		perf_flag=TRUE;
-		time_count=perf_cnt/frames_per_second;        // calculate time per frame based on frequency
-		QueryPerformanceCounter((LARGE_INTEGER *) &next_time);
-
-		unsigned __int64 pf;
-		QueryPerformanceFrequency( (LARGE_INTEGER *)&pf );
-		freq = 1.0 / (double)pf;
-		QueryPerformanceCounter( (LARGE_INTEGER *)& base_time);
-	} 
-	else 
-	{
-		// no performance counter, read in using timeGetTime
-		next_time=timeGetTime();
-	}*/
-
 #if _DEBUG
 	// _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 	// _CrtSetReportMode( _CRT_ERROR, _CRTDBG_MODE_DEBUG );
@@ -98,34 +71,6 @@ int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdL
 			TranslateMessage(&mssg);
 			DispatchMessage(&mssg);
 		} 
-		
-		/*float delta = 0;
-
-		// use the appropriate method to get time
-		if (perf_flag)
-		{
-			QueryPerformanceCounter((LARGE_INTEGER *) &cur_time);
-
-			double milliseconds = ((cur_time - base_time) * freq) * 1000;
-				
-			Timing::Instance()->SetTotalGameTime(milliseconds);
-		}
-		else
-		{
-			cur_time=timeGetTime();
-			Timing::Instance()->SetTotalGameTime(cur_time);
-		}
-
-		// is it time to render and update
-		if (cur_time > next_time) 
-		{
-			// yes, render the frame
-			Update(delta);
-			Render();
-
-			// set time for next frame
-			next_time += time_count;
-		}*/
 
 		if (first_update)
 		{
@@ -140,15 +85,14 @@ int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdL
 		double delta = (currentTime - last_update_time) * 0.001f;
 		timing->SetLastUpdateDelta(delta * timing->GetTimeModifier());
 
-		if (delta > target_delta)
+		if (delta >= target_delta)
 		{
 			Update(delta * timing->GetTimeModifier());
 			last_update_time = currentTime;
 			timing->IncrementTotalTimeSeconds(delta * timing->GetTimeModifier());
-		}
 
-		// draw every frame
-		Render();
+			Render();
+		}
 	}
 
 	return 1;
