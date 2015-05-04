8dc5c4a09ce6eca618d2eb45e93fd114f8fd19d2
jeromebyrne <jeromebyrne21@hotmail.com>
2015-05-03 18:18:15 -0700

    - level editing

--- Game/src/GameObjects/GameObjectManager.cpp
+++ Game/src/GameObjects/GameObjectManager.cpp
@@ -326,13 +326,10 @@ void GameObjectManager::DebugDraw()
 				continue;
 			}
 
-			GAME_ASSERT(dynamic_cast<DrawableObject*>(obj.get()));
-			DrawableObject * drawObj = static_cast<DrawableObject*>(obj.get());
-
 			// only draw if we are in view
-			if (m_camera->IsObjectInView(drawObj))
+			if (m_camera->IsObjectInView(obj.get()))
 			{
-				drawObj->DebugDraw(Graphics::GetInstance()->Device());
+				obj.get()->DebugDraw(Graphics::GetInstance()->Device());
 			}
 		}
 	}
